4ea6d20ce1af13c424ad61df38a658c67d068bfe
SS56Bo <sohamsarkar560@gmail.com>
2025-08-25 15:44:48 +0530

    Cleaner Pipeline + Main Build

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import streamlit as st
+from pipeline import read_pdf, create_chunks, convert_chunks_for_embed, filter_token_count, convert_text_to_embeddings
+st.title("📄 RAG Chatbot")
+st.markdown(
+    """
+    Upload your PDF and let our **Retrieval-Augmented Generation (RAG)** model  
+    build an intelligent chatbot that can answer your questions directly from the document.  
 
-st.title("RAG Application🔎")
+    🔹 No more scrolling through pages  
+    🔹 Just ask, and get answers in seconds  
+    """
+)
+
+store_pdf = st.file_uploader("Upload PDF", type="pdf")
+
+if store_pdf is not None:
+    pdf_content = read_pdf(store_pdf)
+    chunk_content = create_chunks(pdf_content)
+    split_chunks = convert_chunks_for_embed(chunk_content)
+    filtered_split_texts = filter_token_count(split_chunks)
+    embed_text = convert_text_to_embeddings(filtered_split_texts)
+    st.write(embed_text[0])
--- pipeline.py
+++ pipeline.py
@@ -1,31 +1,88 @@
 # Pipeline backend for refactoring code
 import fitz
-import spacy.lang.en as English
+import spacy
+import re
+import pandas as pd
+from sentence_transformers import SentenceTransformer
+import streamlit as st
 
-#creating an object of English
-eng = English()
-eng.add_pipe("sentencizer")
+#creating an object for Spacy Sentencizer
+nlp = spacy.blank('en')
+nlp.add_pipe('sentencizer')
+
+@st.cache_resource
+def load_embed():
+    return SentenceTransformer(model_name_or_path="all-mpnet-base-v2")
+
+embed_model = load_embed()
+
+# a pretty printer
+def format_text(text_in: str)->str:
+    return text_in.replace("\n", " ").strip()
 
 # Function for reading pdf using pymupdf
-def read_pdf(path:str)->list[str]:
-    doc = fitz.open(path)
+def read_pdf(path)->list[dict]:
+    if isinstance(path, str):
+        doc = fitz.open(path)
+    else:
+        doc = fitz.open(stream=path.read(), filetype="pdf")
+    
     content_dict = []
     for page, content in enumerate(doc):
         text = content.get_text()
         text = format_text(text_in=text)
-        content_dict.append({"page_number":page, "page_text": text, "text_length": len(text), "word_count": len(text.split(" "))})
-        # adding sentencizer to create one document at a time
+        doc_nlp = nlp(text)
+        sentences = [sent.text.strip() for sent in doc_nlp.sents]
+        content_dict.append({"page_number":page, 
+                             "page_text": text, 
+                             "text_length": len(text), 
+                             "word_count": len(text.split(" ")),
+                             "sentences": sentences,
+                             "sentence_count": len(sentences)
+                             })
 
     return content_dict
 
-def format_text(text_in: str)->str:
-    return text_in.replace("\n", " ").strip()
+def create_chunks(string_input: list[dict])->list[dict]:
+    for itmz in string_input:
+        itmz["text_chunk"]=convert_sentences_to_chunk(input=itmz["sentences"], split_size=10)
+        itmz["text_chunk_size"]=len(itmz["text_chunk"])
+    
+    return string_input
 
-# conversion into sentences
-def split_into_sentences(list_obj: list[str])->list[str]:
-    for items in list_obj:
-        items["sentence"]=list(eng(items["text"]).sents)
-        items["sentences"]= [str(sentence) for sentence in items["sentences"]]
-        items["sentences_count_spacy"] = len(items["sentences"])
-    return items
+def convert_sentences_to_chunk(input: list[str], split_size: int = 10)->list[list[str]]:
+    '''This function will essentially convert sentences into sentence chunks of size 10 or less, and will return a list containing a list of string'''
+    return [input[i:i+split_size] for i in range(0, len(input), split_size)]
 
+def convert_chunks_for_embed(input_text_info: list[dict])->list[dict]:
+    '''Splitting Chunks for ease of embeddings'''
+    split_chunk = []
+    for items in input_text_info:
+        for parts in items["text_chunk"]:
+            chunk_store = {}
+            chunk_store["pg_no"]=items["page_number"]
+
+            join_sentences_chunks = " ".join(parts).strip()
+            join_sentences_chunks = re.sub(r'\.([A-Z])', r'. \1', join_sentences_chunks)
+
+            chunk_store["chunk_store"]=join_sentences_chunks
+            chunk_store["chunk_store_size"]=len(join_sentences_chunks)
+            chunk_store["chunk_store_word_count"]=len(join_sentences_chunks.split()) # word count
+            chunk_store["chunk_store_token_count"]=len(join_sentences_chunks.split())/4
+
+            split_chunk.append(chunk_store)
+
+    return split_chunk
+     
+def filter_token_count(input_chunk_test: list[dict], min_token_count: int = 15):
+    int_chunk_text = pd.DataFrame(input_chunk_test)
+    filtered_text = int_chunk_text[int_chunk_text["chunk_store_token_count"]>min_token_count]
+    return filtered_text.to_dict(orient="records")
+    
+
+def convert_text_to_embeddings(input_store: list[dict]):
+    '''To convert text into embeddings'''
+    for items in input_store:
+        items["embeddings"]=embed_model.encode(items["chunk_store"])
+    
+    return input_store
